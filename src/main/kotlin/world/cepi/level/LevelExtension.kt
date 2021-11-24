283afa60066b0b7c9dfdd49ab69f08e691c777b8
LeoDog896 <LeoDog896@hotmail.com>
2021-11-24 13:05:06 -0500

    Make actions extension optional

--- src/main/kotlin/world/cepi/level/LevelExtension.kt
+++ src/main/kotlin/world/cepi/level/LevelExtension.kt
@@ -2,6 +2,7 @@ package world.cepi.level
 
 import net.minestom.server.extensions.Extension
 import world.cepi.actions.list.ActionManager
+import world.cepi.kstom.Manager
 import world.cepi.level.actions.ExperienceAction
 import world.cepi.level.actions.LevelAction
 import world.cepi.level.command.LevelCommand
@@ -13,9 +14,13 @@ class LevelExtension : Extension() {
 
         LevelCommand.register()
         IncreaseLevelHook.hook(eventNode)
-        ActionManager.addAll(listOf(LevelAction::class, ExperienceAction::class))
 
         logger.info("[LevelExtension] has been enabled!")
+    }
+
+    override fun postInitialize() {
+        if (Manager.extension.hasExtension("actions"))
+            ActionManager.addAll(listOf(LevelAction::class, ExperienceAction::class))
     }
 
     override fun terminate() {
